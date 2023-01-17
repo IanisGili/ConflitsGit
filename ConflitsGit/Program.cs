31320e9603e8eb53fdaac846b003281848143d89
IanisGili <122864816+IanisGili@users.noreply.github.com>
2023-01-17 11:03:20 +0100

    Update Program.cs

--- ConflitsGit/Program.cs
+++ ConflitsGit/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("Bonjour thomas ");
 =======
 Console.WriteLine("Hola amigo de la barbacoa si si  mario spaghetti");
 >>>>>>> c8320d10abeb67d5b6527ae7c7c9242fc2fd34c3
-Console.WriteLine("Bilel m'a touché"); 
+Console.WriteLine("Bilel m'a touché sensuellement"); 
