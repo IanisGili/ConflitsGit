dc8b4be3ef275d28cd155bc0f70c33250b385186
IanisGili <122864816+IanisGili@users.noreply.github.com>
2023-01-17 11:06:27 +0100

    Update Program.cs

--- ConflitsGit/Program.cs
+++ ConflitsGit/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
+
 Console.WriteLine("Bonjour thomas ");
-=======
+
 Console.WriteLine("Hola amigo de la barbacoa si si  mario spaghetti");
->>>>>>> c8320d10abeb67d5b6527ae7c7c9242fc2fd34c3
-Console.WriteLine("Bilel m'a touché"); 
-Console.WriteLine("enchantié")
+
+Console.WriteLine("Bilel m'a touché sensuellement");
+Console.WriteLine("Bonjour");
