825de0f47b5f6ffd806b34725696781e1441f84b
Che-ese <thomas.vignes@e-artsup.net>
2023-01-17 11:01:05 +0100

    test 2

--- ConflitsGit/Program.cs
+++ ConflitsGit/Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("Bonjour");
 =======
 Console.WriteLine("Hola amigo de la barbacoa si si  mario spaghetti");
 >>>>>>> c8320d10abeb67d5b6527ae7c7c9242fc2fd34c3
+Console.WriteLine("Bilel m'a touché"); 
